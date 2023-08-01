f25d56e0b18c375e106fea062fa990f3d1e49093
Bauxi <valencia.diego825@gmail.com>
2023-08-01 16:00:00 +0200

    addes code and assets

--- coins_counter_game1.py
+++ coins_counter_game1.py
@@ -13,6 +13,18 @@ class PacManGame:
         self.game_over = False
         self.clock = pygame.time.Clock()
 
+        # Load the image files for Pac-Man and the ghost
+        #self.pacman_image = pygame.image.load("pacman.png")
+        self.wall_image = pygame.image.load("assets/WallGraySepia.jpg")
+        self.header_image = pygame.image.load("assets\WoodenBrownBiggerSharpenC.jpg")
+
+        # Resize the images to fit the cell size
+        self.wall_image = pygame.transform.scale(self.wall_image, (self.cell_size, self.cell_size))
+        self.header_image = pygame.transform.scale(self.header_image, (self.cell_size, self.cell_size))
+        
+         # Initialize remaining coins count
+        self.remaining_coins = sum(row.count('.') for row in self.grid)
+        
     def find_pacman(self):
         for y in range(len(self.grid)):
             for x in range(len(self.grid[y])):
@@ -26,20 +38,25 @@ class PacManGame:
         for y in range(len(self.grid)):
             for x in range(len(self.grid[y])):
                 if self.grid[y][x] == '#':
+                    # Use wall image instead of drawing a blue rectangle
+                    screen.blit(self.wall_image, (x * self.cell_size, y * self.cell_size))
+                elif self.grid[y][x] == '#':
                     pygame.draw.rect(screen, (0, 0, 255), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
                 elif self.grid[y][x] == '.':
                     pygame.draw.circle(screen, (255, 255, 0), (x * self.cell_size + self.cell_size // 2, y * self.cell_size + self.cell_size // 2), 4)
                 elif self.grid[y][x] == 'P':
                     pygame.draw.rect(screen, (0, 255, 0), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
+                elif self.grid[y][x] == "*":
+                    screen.blit(self.header_image, (x * self.cell_size, y * self.cell_size))
         pygame.display.update()
 
     def draw_score(self, screen):
-        font = pygame.font.Font(None, 36)
+        font = pygame.font.Font(None, 45)
         score_text = font.render(f"Score: {self.score}", True, (255, 255, 255))
-        score_rect = score_text.get_rect(topleft=(20, self.cell_size + 10))
+        score_rect = score_text.get_rect(topleft=(self.width/2-self.cell_size*2, self.cell_size))
 
         # Draw an unfilled yellow rectangle background for the score text
-        pygame.draw.rect(screen, (255, 255, 0), score_rect, 2)
+        #pygame.draw.rect(screen, (255, 255, 0), score_rect, 4)
 
         # Draw the score text on top of the background rectangle
         screen.blit(score_text, score_rect)
@@ -96,12 +113,25 @@ class PacManGame:
             # Update Pac-Man's current position
             self.grid[self.pacman_y][self.pacman_x] = ' '
             self.pacman_x, self.pacman_y = new_x, new_y
-
+        
+        if self.grid[new_y][new_x] == '.':
+            self.score += 1
+            self.remaining_coins -= 1
+            self.grid[new_y][new_x] = ' '
+        
         # Set the current position to Pac-Man
         self.grid[self.pacman_y][self.pacman_x] = 'P'
 
         # Update the window title with the current score
         pygame.display.set_caption(f"Pac-Man Game - Score: {self.score}")
+    
+    def draw_victory_message(self, screen):
+        font = pygame.font.Font(None, 56)
+        victory_text = font.render("Victory!", True, (255, 255, 255))
+        text_rect = victory_text.get_rect(center=(self.width // 2, self.height // 2))
+        screen.blit(victory_text, text_rect)
+
+        pygame.display.update()
 
 
     def run(self):
@@ -115,35 +145,42 @@ class PacManGame:
             self.draw_grid(screen)
             self.draw_score(screen)  # Add this line to display the score
             self.clock.tick(10)  # Controls the game's frame rate
+            # Display victory message when all coins are collected
+            if self.remaining_coins == 0:
+                self.draw_victory_message(screen)
+                pygame.time.wait(2000)  # Wait for 2 seconds before exiting
+                self.game_over = True
 
         pygame.quit()
         sys.exit()
 
 
+
 if __name__ == "__main__":
     # Your map data here
     custom_map = [
-    "#################################",
-    ".P...............................",
-    "###.#######.#####.#############.#",
-    "#.#.......#...#......#.......#.#",
-    "#.#.#####.#.#####.#####.#####.#.#",
-    "#.#.#...#.#.#..........#...#.#.#",
-    "#.#.#.#.#.#.#.#####.#####.#.#.#.#",
-    "#.#.#.#.#.#.......#.......#.#.#.#",
-    "#.#.#.#.#.#####.#####.#####.#.#.#",
-    "#.#.#.#.#......#...........#.#.#",
-    "#.#.#.#.#####.#########.#####.#.#",
-    "#.#.#.#.....................#.#.#",
-    "#.#.########.#########.###.#.#.#",
-    "#.#.#..........................#",
-    "#.#.#########.###.###.########.#",
-    "#.#.#.......#.....#.....#.....#.#",
-    "#.#.#####.#.#####.#####.#.#####.#",
-    "#.....#...#.....#.#.....#...#...#",
-    "#.###.#.#######.#.#.#######.#.###",
-    "#.....#..........................#",
-    "#################################",
+    "*********************************",
+    "*********************************",    
+    "*###############################*",
+    "*#............#......#.........#*",
+    "*#.###.#####.#.####.#.#####.####*",
+    "*#.###.#####.#.####.#.#####.####*",
+    "*#.............................#*",
+    "*#.###.#.########.########.#.###*",
+    "*#.....#....#..........#.......#*", 
+    "*#######.#.#.######.##.#.#######*",
+    "*#.....#...#..........#...#....#*", 
+    "*#####.#.#.#.########.#.#.#.####*",
+    "*#......#...#..................#*", 
+    "*#.########.#.########.#.#######*",
+    "*#.............................#*",
+    "*###.###.########.########.###.#*",
+    "*#...#.......#.................#*", 
+    "*#.########.#.########.#.#######*",
+    "*#..........#..........#......P#*", 
+    "*###############################*",
+    "*********************************",
 ]
+
     game = PacManGame(custom_map)
     game.run()
